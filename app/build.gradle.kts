6db82f6521ef3811d83f08dd8705b0d6e40a3b08
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2025-06-01 19:35:13 -0300

    Add recycler view library and bind it to fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment)
+    implementation(libs.androidx.viewmodel)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
